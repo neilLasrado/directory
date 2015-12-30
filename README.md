9a501bdea2e45b0020f630e7c32f985b556e9aff
Neil Trini Lasrado <neil@frappe.io>
2015-12-30 23:14:49 +0530

    App init

---
+++ README.md
@@ -0,0 +1,7 @@
+## Directory
+
+Data collection tool for rotary district directory
+
+#### License
+
+MIT
---
+++ directory/hooks.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+app_name = "directory"
+app_title = "Directory"
+app_publisher = "Neil Trini Lasrado"
+app_description = "Data collection tool for rotary district directory"
+app_icon = "octicon octicon-file-directory"
+app_color = "grey"
+app_email = "nlasrado@gmail.com"
+app_version = "0.0.1"
+app_license = "MIT"
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/directory/css/directory.css"
+# app_include_js = "/assets/directory/js/directory.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/directory/css/directory.css"
+# web_include_js = "/assets/directory/js/directory.js"
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+#	"Role": "home_page"
+# }
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# Installation
+# ------------
+
+# before_install = "directory.install.before_install"
+# after_install = "directory.install.after_install"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "directory.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+#	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"directory.tasks.all"
+# 	],
+# 	"daily": [
+# 		"directory.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"directory.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"directory.tasks.weekly"
+# 	]
+# 	"monthly": [
+# 		"directory.tasks.monthly"
+# 	]
+# }
+
+# Testing
+# -------
+
+# before_tests = "directory.install.before_tests"
+
+# Overriding Whitelisted Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "directory.event.get_events"
+# }
+
