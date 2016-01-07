d36934eb02db17de227666be50e7289ca7f68aa9
Neil Trini Lasrado <neil@frappe.io>
2016-01-07 15:55:01 +0530

    Fixed Directory

---
+++ directory/directory/doctype/directory/directory.js
@@ -0,0 +1,16 @@
+frappe.ui.form.on("Directory" ,{
+	onload :function(cur_frm) {
+		if (cur_frm.doc.__islocal){
+			cur_frm.set_value("bod", [
+				{ "designation": "President Elect" },
+				{ "designation": "Treasurer"},
+				{ "designation": "Director Club Service"},
+				{ "designation": "Director Community Service"},
+				{ "designation": "Director Vocational Service"},
+				{ "designation": "Director International Service"},
+				{ "designation": "Director Youth Services"},
+				{ "designation": "Director Public Relations"},
+				{ "designation": "Chairman TRF Support"}]);
+		}
+	}
+});
