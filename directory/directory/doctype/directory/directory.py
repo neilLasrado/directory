316fb04ca2bce5104f93ae5fe993ab5965f13b5d
Neil Trini Lasrado <neil@frappe.io>
2016-01-11 18:38:19 +0530

    added validation to mobile numbers

--- directory/directory/doctype/directory/directory.py
+++ directory/directory/doctype/directory/directory.py
@@ -5,8 +5,37 @@
 from __future__ import unicode_literals
 from frappe.model.document import Document
 from frappe.utils import validate_email_add
+import frappe
+import re
 
 class Directory(Document):
 	def validate(self):
 		validate_email_add(self.email_address.strip(), True)
+		if self.residential_tel:
+			validate_mobile(self.residential_tel)
+		if self.office_tel:
+			validate_mobile(self.office_tel)
+		if self.mobile:
+			validate_mobile(self.mobile)
+		
 		validate_email_add(self.s_email_address.strip(), True)
+		if self.s_residential_number:
+			validate_mobile(self.s_residential_number)
+		if self.s_office_number:
+			validate_mobile(self.s_office_number)
+		if self.s_mobile_number:
+			validate_mobile(self.s_mobile_number)
+
+		for d in self.bod:
+			if d.email_address:
+				validate_email_add(d.email_address.strip(), True)
+				
+			if d.mobile_number:
+				validate_mobile(d.mobile_number)
+
+
+def validate_mobile(value):
+	rule = re.compile(r'^[0-9]*$')
+	
+	if not rule.search(value):
+		frappe.throw("Invalid mobile number - {0}".format(value))
