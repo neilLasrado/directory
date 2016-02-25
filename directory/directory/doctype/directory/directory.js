1cbe8b8582e31c12a067f8b880deb38639e33fa3
Neil Trini Lasrado <neil@frappe.io>
2016-02-25 12:36:09 +0530

    Fixes in directory

--- directory/directory/doctype/directory/directory.js
+++ directory/directory/doctype/directory/directory.js
@@ -2,10 +2,12 @@ frappe.ui.form.on("Directory" ,{
 	onload :function(cur_frm) {
 		if (cur_frm.doc.__islocal){
 			cur_frm.set_value("bod", [
+				{ "designation": "IPP"},
 				{ "designation": "President Elect"},
 				{ "designation": "Treasurer"},
 				{ "designation": "Director Club Service"},
-				{ "designation": "Director Community Service"},
+				{ "designation": "Director Community Service - Medical"},
+				{ "designation": "Director Community Service - Non Medical"},
 				{ "designation": "Director Vocational Service"},
 				{ "designation": "Director International Service"},
 				{ "designation": "Director Youth Services"},
